7d97303fc9afcbbf3f384adcbdc493b557d4ae2e
Aheiy1 <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>
2022-03-18 13:34:30 -0500

    React Boilder Plate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Main } from "./components";
+
+ReactDom.render(
+  <Router>
+    <Main />
+  </Router>,
+  document.getElementById("app")
+);
